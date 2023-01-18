fd128d016f058359dca13ec0c0e7ffd4e2ec004c
Michael Wan <hkmicmic@gmail.com>
2023-01-18 18:08:42 +0000

    Update on moneytab webbot

--- app/src/main/java/io/github/micwan88/moneytab/WebBot.java
+++ app/src/main/java/io/github/micwan88/moneytab/WebBot.java
@@ -1,7 +1,18 @@
 package io.github.micwan88.moneytab;
 
+import java.util.Properties;
+
+import io.github.micwan88.helperclass4j.AppPropertiesUtil;
+
 public class WebBot {
 	public String dummyTest() {
 		return "Test";
+	}
+	
+	public static void main(String[] args) {
+		Properties appProperties = AppPropertiesUtil.loadProperties(AppPropertiesUtil.APP_PROPERTY_FILE);
+		if (appProperties == null) {
+			
+		}
 	}
 }
