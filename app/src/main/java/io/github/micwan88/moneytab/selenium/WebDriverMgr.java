3fc82424a77621701be67da6a6faacfa6bc8586e
Michael Wan <hkmicmic@gmail.com>
2023-01-25 21:39:57 +0000

    organize import

--- app/src/main/java/io/github/micwan88/moneytab/selenium/WebDriverMgr.java
+++ app/src/main/java/io/github/micwan88/moneytab/selenium/WebDriverMgr.java
@@ -19,7 +19,6 @@ import org.openqa.selenium.WebDriver.Options;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class WebDriverMgr {
