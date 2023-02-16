95a5ab9621e44ce06e032f07012047c7e9d01af7
Michael Wan <hkmicmic@gmail.com>
2023-02-16 22:05:32 +0000

    add deploy script for docker

---
+++ docker-build/deploy-webbot.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -o pipefail
+
+WEBBOT_VERSION=1.0.0-SNAPSHOT
+REPO_DIR=${WORKDIR}/moneytab-webbot
+
+# getting source
+git clone https://github.com/micwan88/moneytab-webbot.git
+
+cd ${REPO_DIR}
+
+# build the artifact
+gradle clean build -x test
+
+# stop the gradle after build
+gradle --stop
+
+# copy artifact to working folder
+cp app/build/distributions/moneytab-webbot-${WEBBOT_VERSION}.zip ${WORKDIR}/
+
+cd ${WORKDIR}
+
+# unzip artifact
+unzip moneytab-webbot-${WEBBOT_VERSION}.zip
+
+# make link with libs
+ln -s moneytab-webbot-${WEBBOT_VERSION}/lib lib
+
+# copy required files
+cp moneytab-webbot-${WEBBOT_VERSION}/*.sh .
+cp moneytab-webbot-${WEBBOT_VERSION}/app.properties .
+cp moneytab-webbot-${WEBBOT_VERSION}/log4j2.xml .
